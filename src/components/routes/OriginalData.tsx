476f64ec1de47736c47103b75447dd76b916a1f6
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2022-12-04 19:16:06 +0800

    1

--- src/components/routes/OriginalData.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../App.css';
-import {OriginalData} from '../pages/OriginalData';
-export const Home = () => {
-    return (
-        <OriginalData />
-    );
-};
