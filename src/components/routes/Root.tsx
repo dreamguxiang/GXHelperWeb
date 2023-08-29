62e7151ccef23d7a4e005077c53bf8a7e2e76ba9
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2023-08-29 18:45:06 +0800

    1

--- src/components/routes/Root.tsx
+++ src/components/routes/Root.tsx
@@ -1,85 +1,9 @@
-import React, { useState } from 'react';
-import '../App.css';
-import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-} from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
-import {NavLink,useLocation, useRoutes} from "react-router-dom";
-import {Home} from '../routes/Home';
-import {OriginalDataInit} from "./OriginalData";
-import {PackEncryptedInit} from "./PackEncrypted";
 
-const { Header, Sider, Footer } = Layout;
+import {OriginalDataInit} from "./OriginalData";
+
 
 export const Root = () => {
-    const [collapsed, setCollapsed] = useState(false);
-    const  location = useLocation();
-
-    const GetRoutes = () => {
-        const routes = useRoutes([
-            {
-                path:'/originaldata',
-                element: <OriginalDataInit />,
-            },
-            {
-                path:'/PackEncrypted',
-                element: <PackEncryptedInit />,
-            },
-            {
-                path:'/',
-                element: <Home />
-            }
-        ]);
-        return routes;
-    }
-
-    return (
-        <Layout>
-            <Sider trigger={null} collapsible collapsed={collapsed}>
-                <div className="logo" />
-                <Menu
-                    theme="dark"
-                    mode="inline"
-                    selectedKeys={[location.pathname]}
-                    items={[
-                        {
-                            key: '/',
-                            icon: <UserOutlined />,
-                            label: (
-                                <NavLink to="/" >Home</NavLink>
-                            ),
-                        },
-                        {
-                            key: '/OriginalData',
-                            icon: <VideoCameraOutlined />,
-                            label: (
-                                <NavLink to="/OriginalData" >BDS Original Data</NavLink>
-                            ),
-                        },
-                        {
-                            key: '/PackEncrypted',
-                            icon: <UploadOutlined />,
-                            label: (
-                                <NavLink to="/PackEncrypted" >Pack Encrypted</NavLink>
-                            ),
-                        },
-                    ]}
-                />
-            </Sider>
-            <Layout className="site-layout">
-                <Header className="site-layout-background" style={{ padding: 0 }}>
-                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                        className: 'trigger',
-                        onClick: () => setCollapsed(!collapsed),
-                    })}
-                </Header>
-                <GetRoutes />
-                <Footer style={{ textAlign: 'center' }}>GXHelper ©2022 Created by QingYu</Footer>
-            </Layout>
-        </Layout>
+    return (      
+            <OriginalDataInit />    
     );
 };
